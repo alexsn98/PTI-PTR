915229d45afc3efdd70ef06a7288c006cec21b6e
alexNasc <alexmento98@gmail.com>
2019-03-19 16:57:32 +0000

    inicio da implementacao de css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href= {{ asset('css/loginPage.css') }}>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -11,8 +12,8 @@
     <title>Login Page</title>
 </head>
 <body class="mw-100 bg-info">
-    <div id="login" class="p-5 mx-auto bg-danger" style="width: 40%; margin-top:10%;">
-
+    <div id="login" class="p-5 mx-auto bg-light">
+        <img src={{ asset('img/logo.png') }}>
         <form method="POST" action="/">
             @csrf
             
