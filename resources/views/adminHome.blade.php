9baef5c9913ba1cf86d726791cd952987ab8dd17
Rodrigo Alcarva <roal@live.com.pt>
2019-04-28 10:42:14 +0000

    Pagina css Admin Home acabada

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -10,10 +10,28 @@
     </div>
     <div id="container">
         <div id="divUser">
+            <div class="image">
+                <img src={{asset('img/user.png')}}>
+            </div>
+            <div class="text">
+                <h2>Utilizadores</h2>
+            </div>
         </div>
         <div id="divCurso">
+            <div class="image">
+                <img src={{asset('img/curso.png')}}>
+            </div>
+            <div class="text">
+                <h2>Cursos</h2>
+            </div>
         </div>
         <div id="divCadeira">
+            <div class="image">
+                <img src={{asset('img/cadeira.png')}}>
+            </div>
+            <div class="text">
+                <h2>Cadeiras</h2>
+            </div>
         </div>
     </div>
 @endsection
