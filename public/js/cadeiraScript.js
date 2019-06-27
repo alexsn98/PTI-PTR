d495073d323666c4723da2bd37de4fb8adc503da
alexNasc <alexmento98@gmail.com>
2019-06-27 14:30:00 +0100

    docente fechar turmas

--- public/js/cadeiraScript.js
+++ public/js/cadeiraScript.js
@@ -1,10 +1,12 @@
 function selecionarTurma(turmaId) {
     let docente = document.getElementById('left').getElementsByTagName('h4')[0];
+    let horarioDuvidas = document.getElementById('left').getElementsByTagName('h4')[1];
     let tipo = document.getElementById('right').getElementsByTagName('h4')[0];
     let aulasTipo = document.getElementById('view1').getElementsByTagName('div')[0].getElementsByTagName('ul')[0];
     let semAulasTipo = document.getElementById('view1').getElementsByTagName('div')[0].getElementsByTagName('h3')[0];
     let inscreverTurma = document.getElementById('inscreverTurmaButton'); //para aluno
     let paginaTurma = document.getElementById('paginaTurmaButton'); //para admin
+    let fecharTurma = document.getElementById('fecharTurmaButton'); //para admin
 
     let url = '/home/turmaInfo/' + turmaId;
 
@@ -17,6 +19,7 @@ function selecionarTurma(turmaId) {
         if (this.readyState == 4 && this.status == 200) {
             //limpa pagina
             docente.textContent = 'Professor:'
+            horarioDuvidas.textContent = 'Horário dúvidas:'
             tipo.textContent = 'Tipo:';
 
             if (inscreverTurma != null) inscreverTurma.style.display = "none";
@@ -32,11 +35,18 @@ function selecionarTurma(turmaId) {
             if (resposta.tipo == 1) turmaTipo = "Teórica";
 
             docente.textContent += ' ' + resposta.docente; 
+            
+            horarioDuvidas.textContent += ' ' + resposta.horarioDuvidas; 
             tipo.textContent += ' ' + turmaTipo;
 
             if (paginaTurma != null && resposta.estado == 'comVagas') {
                 paginaTurma.style.display = "block";
-                paginaTurma.setAttribute("href", "/home/cadeira/turma/" + turmaId)
+                paginaTurma.setAttribute("href", "/home/cadeira/turma/" + turmaId);
+            }
+
+            if (resposta.eRegente) {
+                fecharTurma.style.display = "block";
+                paginaTurma.setAttribute("href", "/home/cadeira/fecharTurma/" + turmaId);
             }
                         
             if (resposta.aulasTipo.length > 0) {
